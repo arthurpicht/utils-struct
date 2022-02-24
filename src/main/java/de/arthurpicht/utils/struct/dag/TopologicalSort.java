c9d5f2cae64222afe0eca8064d248c581544eab9
Arthur Picht <picht@arthurpicht.de>
2022-02-24 17:50:27 +0100

    TopologicalSort cleaned

--- src/main/java/de/arthurpicht/utils/struct/dag/TopologicalSort.java
+++ src/main/java/de/arthurpicht/utils/struct/dag/TopologicalSort.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class TopologicalSort<N> {
 
-    private static class StackNode<N> {
+    private static final class StackNode<N> {
 
         private final N node;
         private final NodeStatus nodeStatus;
@@ -34,23 +34,23 @@ public class TopologicalSort<N> {
             return node;
         }
 
-        public NodeStatus getNodeStatus() {
-            return nodeStatus;
-        }
-
         @Override
         public String toString() {
             return this.node.toString() + ":" + this.nodeStatus.toString();
         }
     }
 
-    private enum NodeStatus { NEW, IN_PROCESS, FINISHED };
+    private enum NodeStatus { NEW, IN_PROCESS, FINISHED }
 
     private final Dag<N> dag;
     private final N startNode;
+    private final List<N> topologicalSortedNodes;
 
     private final Set<Edge<N>> mutedEdges;
-    private final List<N> topologicalSortedNodes;
+    private final Stack<StackNode<N>> nodesStack;
+    private StackNode<N> currentStackNode;
+    private StackNode<N> lastVisitedStackNode;
+    private N currentNode;
 
     public static <N> List<N> get(Dag<N> dag, N startNode) {
         return new TopologicalSort<N>(dag, startNode).getTopologicalSortedNodes();
@@ -62,99 +62,109 @@ public class TopologicalSort<N> {
         this.startNode = startNode;
 
         this.mutedEdges = new HashSet<>();
+        this.nodesStack = new Stack<>();
+        this.lastVisitedStackNode = null;
+        this.currentStackNode = null;
+        this.currentNode = null;
+
         this.topologicalSortedNodes = new ArrayList<>();
 
         sort();
     }
 
     private void sort() {
-        Stack<StackNode<N>> nodesStack = new Stack<>();
-        nodesStack.push(StackNode.createInstanceNew(this.startNode));
-        StackNode<N> lastVisitedStackNode = null;
 
-        int counter = 0;
+        if (this.dag.getDownstreamNodes(this.startNode).isEmpty()) {
+            this.topologicalSortedNodes.add(this.startNode);
+            return;
+        }
+
+        nodesStack.push(StackNode.createInstanceNew(this.startNode));
 
         while (!nodesStack.empty()) {
 
-            counter++;
-            if (counter > 20) return;
-
-            StackNode<N> currentStackNode = nodesStack.pop();
-            N currentNode = currentStackNode.getNode();
+            this.currentStackNode = nodesStack.pop();
+            this.currentNode = currentStackNode.getNode();
 
             System.out.println("Process         : " + currentStackNode);
 
-            Set<N> nodeOptions = getTraversalOptions(currentNode);
+            Set<N> nodeOptions = getTraversalOptions();
 
             System.out.println("nodeStack       : " + Strings.listing(Collections.list(nodesStack.elements()), ", "));
             System.out.println("currentNode     : " + currentNode);
             System.out.println("lastVisitedNode : " + lastVisitedStackNode);
             System.out.println("nodeOptions     : " + Strings.listing(nodeOptions, ", "));
-            System.out.println("mutedEdges      : " + Strings.listing(this.mutedEdges, ", "));
+            System.out.println("mutedEdges      : " + Strings.listing(mutedEdges, ", "));
 
             if (currentStackNode.nodeStatus == NodeStatus.NEW) {
+
                 if (nodeOptions.isEmpty()) {
-                    System.out.println("No downstream edges.");
-                    addToSorted(currentNode);
-
-                    Edge<N> edgeToMute = new Edge<>(lastVisitedStackNode.node, currentNode);
-                    this.mutedEdges.add(edgeToMute);
-                    System.out.println("mute edge       : " + edgeToMute);
-
-                    lastVisitedStackNode = currentStackNode;
+                    processPrimarySink();
                 } else {
-                    System.out.println("With downstream edges.");
-                    N someOption = Sets.getSomeElement(nodeOptions);
-                    nodesStack.push(StackNode.createInstanceInProcess(currentNode));
-                    nodesStack.push(StackNode.createInstanceNew(someOption));
-
-                    System.out.println("push to stack   : " + currentNode + ":IN_PROCESS");
-                    System.out.println("push to stack   : " + someOption + ":NEW");
-
-                    lastVisitedStackNode = currentStackNode;
+                    processNodeWithDownstreamEdges(nodeOptions);
                 }
+                lastVisitedStackNode = currentStackNode;
 
             } else if (currentStackNode.nodeStatus == NodeStatus.IN_PROCESS){
-                if (lastVisitedStackNode.nodeStatus == NodeStatus.FINISHED) {
-                    System.out.println("Process finished Node: " + lastVisitedStackNode.node);
-                    Edge<N> edge = new Edge<>(currentNode, lastVisitedStackNode.node);
-                    this.mutedEdges.add(edge);
-                    System.out.println("mute edge       : " + edge);
-                    nodeOptions.remove(lastVisitedStackNode.node);
-                }
 
+                if (lastVisitedStackNode == null) throw new IllegalStateException();
+                if (lastVisitedStackNode.nodeStatus == NodeStatus.FINISHED) {
+                    processFinishedNode(nodeOptions);
+                }
 
                 if (nodeOptions.isEmpty()) {
-                    System.out.println("No downstream edges.");
-                    addToSorted(currentNode);
-
-                    Edge<N> edgeToMute = new Edge<>(currentNode, lastVisitedStackNode.node);
-                    this.mutedEdges.add(edgeToMute);
-                    System.out.println("mute edge       : " + edgeToMute);
-
+                    processSecondarySink();
                     lastVisitedStackNode = StackNode.createInstanceFinished(currentStackNode.node);
                 } else {
-                    System.out.println("With downstream edges.");
-
-                    N someOption = Sets.getSomeElement(nodeOptions);
-                    nodesStack.push(StackNode.createInstanceInProcess(currentNode));
-                    nodesStack.push(StackNode.createInstanceNew(someOption));
-
-                    System.out.println("push to stack   : " + currentNode + ":IN_PROCESS");
-                    System.out.println("push to stack   : " + someOption + ":NEW");
-
+                    processNodeWithDownstreamEdges(nodeOptions);
                     lastVisitedStackNode = currentStackNode;
                 }
-            } else if (currentStackNode.nodeStatus == NodeStatus.FINISHED) {
-                System.out.println("FINISHED!");
             }
 
 
             System.out.println("sorted nodes    : " + Strings.listing(this.topologicalSortedNodes, ", "));
-
             System.out.println("----------------------------------");
-
         }
+    }
+
+    private void processPrimarySink() {
+        processSink(false);
+    }
+
+    private void processSecondarySink() {
+        processSink(true);
+    }
+
+    private void processSink(boolean backDirection) {
+        System.out.println("No downstream edges.");
+        addToSorted(this.currentNode);
+        if (this.lastVisitedStackNode == null) throw new IllegalStateException();
+        Edge<N> edgeToMute;
+        if (backDirection) {
+            edgeToMute = new Edge<>(currentNode, this.lastVisitedStackNode.node);
+        } else {
+            edgeToMute = new Edge<>(this.lastVisitedStackNode.node, currentNode);
+        }
+        this.mutedEdges.add(edgeToMute);
+        System.out.println("mute edge       : " + edgeToMute);
+    }
+
+    private void processNodeWithDownstreamEdges(Set<N> nodeOptions) {
+        System.out.println("With downstream edges.");
+        N someOption = Sets.getSomeElement(nodeOptions);
+        this.nodesStack.push(StackNode.createInstanceInProcess(this.currentNode));
+        this.nodesStack.push(StackNode.createInstanceNew(someOption));
+
+        System.out.println("push to stack   : " + this.currentNode + ":IN_PROCESS");
+        System.out.println("push to stack   : " + someOption + ":NEW");
+    }
+
+    private void processFinishedNode(Set<N> nodeOptions) {
+        System.out.println("Process finished Node: " + this.lastVisitedStackNode.node);
+        Edge<N> edge = new Edge<>(this.currentNode, this.lastVisitedStackNode.node);
+        this.mutedEdges.add(edge);
+        System.out.println("mute edge       : " + edge);
+        nodeOptions.remove(this.lastVisitedStackNode.node);
     }
 
     private void addToSorted(N node) {
@@ -168,9 +178,9 @@ public class TopologicalSort<N> {
         return Collections.unmodifiableList(this.topologicalSortedNodes);
     }
 
-    private Set<N> getTraversalOptions(N node) {
-        return this.dag.getDownstreamNodes(node).stream()
-                .map(n -> new Edge<>(node, n))
+    private Set<N> getTraversalOptions() {
+        return this.dag.getDownstreamNodes(this.currentNode).stream()
+                .map(n -> new Edge<>(this.currentNode, n))
                 .filter(n -> !this.mutedEdges.contains(n))
                 .map(Edge::getTo)
                 .collect(Collectors.toSet());
--- src/test/java/de/arthurpicht/utils/struct/dag/TopologicalSortTest.java
+++ src/test/java/de/arthurpicht/utils/struct/dag/TopologicalSortTest.java
@@ -63,8 +63,6 @@ class TopologicalSortTest {
         check(topologicalSortedNodes, dag);
     }
 
-
-
     @Test
     void tree() {
         Dag<String> dag = new DagBuilder<String>()
@@ -84,5 +82,102 @@ class TopologicalSortTest {
         List<String> topologicalSortedNodes = TopologicalSort.get(dag, "A");
         check(topologicalSortedNodes, dag);
     }
+
+    @Test
+    void tree2() {
+        Dag<String> dag = new DagBuilder<String>()
+                .withNode("A")
+                .withNode("B1")
+                .withNode("B2")
+                .withNode("C1")
+                .withNode("C2")
+                .withNode("C3")
+                .withNode("D1")
+                .withNode("D2")
+                .withEdge("A", "B1")
+                .withEdge("A", "B2")
+                .withEdge("B1", "C1")
+                .withEdge("B1", "C2")
+                .withEdge("B1", "C3")
+                .withEdge("C2", "D1")
+                .withEdge("C2", "D2")
+                .build();
+
+        List<String> topologicalSortedNodes = TopologicalSort.get(dag, "A");
+        check(topologicalSortedNodes, dag);
+    }
+
+    @Test
+    void tree2WithShortcut() {
+        Dag<String> dag = new DagBuilder<String>()
+                .withNode("A")
+                .withNode("B1")
+                .withNode("B2")
+                .withNode("C1")
+                .withNode("C2")
+                .withNode("C3")
+                .withNode("D1")
+                .withNode("D2")
+                .withEdge("A", "B1")
+                .withEdge("A", "B2")
+                .withEdge("B1", "C1")
+                .withEdge("B1", "C2")
+                .withEdge("B1", "C3")
+                .withEdge("C2", "D1")
+                .withEdge("C2", "D2")
+                .withEdge("B2", "D2")
+                .build();
+
+        List<String> topologicalSortedNodes = TopologicalSort.get(dag, "A");
+        check(topologicalSortedNodes, dag);
+    }
+
+    @Test
+    void tree2WithShortcutReverse() {
+        Dag<String> dag = new DagBuilder<String>()
+                .withNode("A")
+                .withNode("B1")
+                .withNode("B2")
+                .withNode("C1")
+                .withNode("C2")
+                .withNode("C3")
+                .withNode("D1")
+                .withNode("D2")
+                .withEdge("A", "B1")
+                .withEdge("A", "B2")
+                .withEdge("B2", "C1")
+                .withEdge("B2", "C2")
+                .withEdge("B2", "C3")
+                .withEdge("C2", "D1")
+                .withEdge("C2", "D2")
+                .withEdge("B1", "D2")
+                .build();
+
+        List<String> topologicalSortedNodes = TopologicalSort.get(dag, "A");
+        check(topologicalSortedNodes, dag);
+    }
+
+    @Test
+    void justOneNode() {
+        Dag<String> dag = new DagBuilder<String>()
+                .withNode("A")
+                .build();
+
+        List<String> topologicalSortedNodes = TopologicalSort.get(dag, "A");
+        check(topologicalSortedNodes, dag);
+    }
+
+    @Test
+    void twoNodesChain() {
+        Dag<String> dag = new DagBuilder<String>()
+                .withNode("A")
+                .withNode("B")
+                .withEdge("A", "B")
+                .build();
+
+        List<String> topologicalSortedNodes = TopologicalSort.get(dag, "A");
+        check(topologicalSortedNodes, dag);
+    }
+
 
 }
